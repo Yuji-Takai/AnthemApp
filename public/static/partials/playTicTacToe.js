7ac24c072ccaa5bc74d2631dbd1dd361e5c15b7a
yujitakai <yuji21eagles0912@yahoo.co.jp>
2018-03-24 15:21:12 -0400

    updated

--- public/static/partials/playTicTacToe.js
+++ public/static/partials/playTicTacToe.js
@@ -21,96 +21,97 @@ function imageChoosing(num1) {
 
 function playTicTacToe(num2) {
     if (image == "nothing") {
-    } else if (num2 == 1) {
-        document.getElementById("r11").src=image;
-        elements[0] = image;
-    } else if (num2 == 2) {
-        document.getElementById("r12").src=image;
-        elements[1] = image;
-    } else if (num2 == 3) {
-        document.getElementById("r13").src=image;
-        elements[2] = image;
-    } else if (num2 == 4) {
-        document.getElementById("r21").src=image;
-        elements[3] = image;
-    } else if (num2 == 5) {
-        document.getElementById("r22").src=image;
-        elements[4] = image;
-    } else if (num2 == 6) {
-        document.getElementById("r23").src=image;
-        elements[5] = image;
-    } else if (num2 == 7) {
-        document.getElementById("r31").src=image;
-        elements[6] = image;
-    } else if (num2 == 8) {
-        document.getElementById("r32").src=image;
-        elements[7] = image;
     } else {
-        document.getElementById("r33").src=image;
-        elements[8] = image;
-    }
-    document.getElementById("button11").disabled = true;
-    document.getElementById("button12").disabled = true;
-    document.getElementById("button13").disabled = true;
-    document.getElementById("button21").disabled = true;
-    document.getElementById("button22").disabled = true;
-    document.getElementById("button23").disabled = true;
-    document.getElementById("button31").disabled = true;
-    document.getElementById("button32").disabled = true;
-    document.getElementById("button33").disabled = true;
-    if (elements[0] == image
-        && ((elements[0] == elements[1] && elements[1] == elements[2])
-            || (elements[0] == elements[3] && elements[3] == elements[6])
-            || (elements[0] == elements[4] && elements[4] == elements[8]))) {
-        document.getElementById("result").innerHTML = "YOU WIN!";
-    } else if (elements[1] == image
-        && (elements[1] == elements[4] && elements[4] == elements[7])) {
-        document.getElementById("result").innerHTML = "YOU WIN!";
-    } else if (elements[2] == image
-        && ((elements[2] == elements[5] && elements[5] == elements[8])
-            || (elements[2] == elements[4] && elements[4] == elements[6]))) {
-        document.getElementById("result").innerHTML = "YOU WIN!";
-    } else if (elements[3] == image
-        && (elements[3] == elements[4] && elements[4] == elements[5])) {
-        document.getElementById("result").innerHTML = "YOU WIN!";
-    } else if (elements[6] == image
-        && (elements[6] == elements[7] && elements[7] == elements[8])) {
-        document.getElementById("result").innerHTML = "YOU WIN!";
-    } else {
-        computerPlaying();
-        if (elements[0] == "cross.png"
+        if (num2 == 1) {
+            document.getElementById("r11").src=image;
+            elements[0] = image;
+        } else if (num2 == 2) {
+            document.getElementById("r12").src=image;
+            elements[1] = image;
+        } else if (num2 == 3) {
+            document.getElementById("r13").src=image;
+            elements[2] = image;
+        } else if (num2 == 4) {
+            document.getElementById("r21").src=image;
+            elements[3] = image;
+        } else if (num2 == 5) {
+            document.getElementById("r22").src=image;
+            elements[4] = image;
+        } else if (num2 == 6) {
+            document.getElementById("r23").src=image;
+            elements[5] = image;
+        } else if (num2 == 7) {
+            document.getElementById("r31").src=image;
+            elements[6] = image;
+        } else if (num2 == 8) {
+            document.getElementById("r32").src=image;
+            elements[7] = image;
+        } else {
+            document.getElementById("r33").src=image;
+            elements[8] = image;
+        }
+        document.getElementById("button11").disabled = true;
+        document.getElementById("button12").disabled = true;
+        document.getElementById("button13").disabled = true;
+        document.getElementById("button21").disabled = true;
+        document.getElementById("button22").disabled = true;
+        document.getElementById("button23").disabled = true;
+        document.getElementById("button31").disabled = true;
+        document.getElementById("button32").disabled = true;
+        document.getElementById("button33").disabled = true;
+        if (elements[0] == image
             && ((elements[0] == elements[1] && elements[1] == elements[2])
                 || (elements[0] == elements[3] && elements[3] == elements[6])
                 || (elements[0] == elements[4] && elements[4] == elements[8]))) {
-            document.getElementById("result").innerHTML = "YOU LOSE...";
-        } else if (elements[1] == "cross.png"
+            document.getElementById("result").innerHTML = "YOU WIN!";
+        } else if (elements[1] == image
             && (elements[1] == elements[4] && elements[4] == elements[7])) {
-            document.getElementById("result").innerHTML = "YOU LOSE...";
-        } else if (elements[2] == "cross.png"
+            document.getElementById("result").innerHTML = "YOU WIN!";
+        } else if (elements[2] == image
             && ((elements[2] == elements[5] && elements[5] == elements[8])
                 || (elements[2] == elements[4] && elements[4] == elements[6]))) {
-            document.getElementById("result").innerHTML = "YOU LOSE...";
-        } else if (elements[3] == "cross.png"
+            document.getElementById("result").innerHTML = "YOU WIN!";
+        } else if (elements[3] == image
             && (elements[3] == elements[4] && elements[4] == elements[5])) {
-            document.getElementById("result").innerHTML = "YOU LOSE...";
-        } else if (elements[6] == "cross.png"
+            document.getElementById("result").innerHTML = "YOU WIN!";
+        } else if (elements[6] == image
             && (elements[6] == elements[7] && elements[7] == elements[8])) {
-            document.getElementById("result").innerHTML = "YOU LOSE...";
-        } else if (!elements.includes("white.png")) {
-            document.getElementById("result").innerHTML = "ITS A DRAW!";
+            document.getElementById("result").innerHTML = "YOU WIN!";
         } else {
-            document.getElementById("button11").disabled = false;
-            document.getElementById("button12").disabled = false;
-            document.getElementById("button13").disabled = false;
-            document.getElementById("button21").disabled = false;
-            document.getElementById("button22").disabled = false;
-            document.getElementById("button23").disabled = false;
-            document.getElementById("button31").disabled = false;
-            document.getElementById("button32").disabled = false;
-            document.getElementById("button33").disabled = false;
+            computerPlaying();
+            if (elements[0] == "cross.png"
+                && ((elements[0] == elements[1] && elements[1] == elements[2])
+                    || (elements[0] == elements[3] && elements[3] == elements[6])
+                    || (elements[0] == elements[4] && elements[4] == elements[8]))) {
+                document.getElementById("result").innerHTML = "YOU LOSE...";
+            } else if (elements[1] == "cross.png"
+                && (elements[1] == elements[4] && elements[4] == elements[7])) {
+                document.getElementById("result").innerHTML = "YOU LOSE...";
+            } else if (elements[2] == "cross.png"
+                && ((elements[2] == elements[5] && elements[5] == elements[8])
+                    || (elements[2] == elements[4] && elements[4] == elements[6]))) {
+                document.getElementById("result").innerHTML = "YOU LOSE...";
+            } else if (elements[3] == "cross.png"
+                && (elements[3] == elements[4] && elements[4] == elements[5])) {
+                document.getElementById("result").innerHTML = "YOU LOSE...";
+            } else if (elements[6] == "cross.png"
+                && (elements[6] == elements[7] && elements[7] == elements[8])) {
+                document.getElementById("result").innerHTML = "YOU LOSE...";
+            } else if (!elements.includes("white.png")) {
+                document.getElementById("result").innerHTML = "ITS A DRAW!";
+            } else {
+                document.getElementById("button11").disabled = false;
+                document.getElementById("button12").disabled = false;
+                document.getElementById("button13").disabled = false;
+                document.getElementById("button21").disabled = false;
+                document.getElementById("button22").disabled = false;
+                document.getElementById("button23").disabled = false;
+                document.getElementById("button31").disabled = false;
+                document.getElementById("button32").disabled = false;
+                document.getElementById("button33").disabled = false;
+            }
         }
     }
-
 }
 playTicTacToe();
 
